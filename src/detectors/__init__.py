e80f03a3d163f0531402bc0e246830f216462610
marco-prezpad <marcoperezpadilla2003@gmail.com>
2026-08-10 16:55:52 +0100

    #4. Detector base interface

---
+++ src/detectors/__init__.py
@@ -0,0 +1,12 @@
+"""
+src/detectors/__init__.py
+
+Drift detector wrappers for ShiftBench.
+
+Author: Marco Pérez Padilla
+Date:   10-08-2026
+"""
+
+from .base import BaseDetector
+
+__all__ = ["BaseDetector"]
